5d8dfecd22721975434c430d14f7eb85e3fb0513
Alfimadani <alfimadani100@gmail.com>
2021-09-14 17:11:43 +0800

    melakukan modifikasi pada MyMigration.php menambahkan field

--- app/Database/Migrations/2021-09-14-084928_MyMigration.php
+++ app/Database/Migrations/2021-09-14-084928_MyMigration.php
@@ -8,11 +8,28 @@ class MyMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'blog_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'blog_title'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'blog_description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('blog');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('blog');
     }
 }
