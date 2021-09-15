2f033c5fe73785001b9ffe24ddcee18247c7655f
Alfimadani <alfimadani100@gmail.com>
2021-09-15 08:43:08 +0800

    melakukan penambahan content pada UserId atau telah diisi filed

--- app/Database/Migrations/2021-09-15-003126_UserId.php
+++ app/Database/Migrations/2021-09-15-003126_UserId.php
@@ -8,11 +8,24 @@ class UserId extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 33,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255'
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user');
     }
 }
