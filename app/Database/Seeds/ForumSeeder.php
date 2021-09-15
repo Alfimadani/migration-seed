0105b8e04a7a5071667493d2e600c83d372f5a53
Alfimadani <alfimadani100@gmail.com>
2021-09-15 14:03:24 +0800

    menambahkan faker dalam forumSeeder

--- app/Database/Seeds/ForumSeeder.php
+++ app/Database/Seeds/ForumSeeder.php
@@ -8,6 +8,14 @@ class ForumSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $data = [
+                'forum' => $faker->creditCardType(),
+                'user_id' => $faker->unique()->randomDigit()
+            ];
+            // Using Query Builder
+            $this->db->table('forum')->insert($data);
+        }
     }
 }
