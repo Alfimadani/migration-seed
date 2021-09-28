93bea2014b56b3314dfe65aed59b6759ee9ee32c
Alfimadani <alfimadani100@gmail.com>
2021-09-28 10:45:33 +0800

    menambahkan stdCourse stdCourse.php

---
+++ app/Database/Migrations/migrations_belajar/2021-09-28-023935_StdCourse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations\migrations_belajar;
+
+use CodeIgniter\Database\Migration;
+
+class StdCourse extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'stdnt_course_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 15,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'student_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 15,
+                'unsigned'       => true,
+            ],
+            'course_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 15,
+                'unsigned'       => true,
+            ],
+        ]);
+        $this->forge->addKey('stdnt_course_id', true);
+        $this->forge->createTable('course_name');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('course_name');
+    }
+}
