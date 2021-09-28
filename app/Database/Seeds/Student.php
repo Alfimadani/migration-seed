f8aa7a37ab3dfa1905b0220f26471a6633074b85
Alfimadani <alfimadani100@gmail.com>
2021-09-28 11:31:42 +0800

    melakukan penambahan data faker

---
+++ app/Database/Seeds/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Student extends Seeder
+{
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 5; $i++) {
+            $data = [
+                'name' => $faker->name(),
+                'email'    => $faker->email()
+            ];
+            // Using Query Builder
+            $this->db->table('users')->insert($data);
+        }
+    }
+}
