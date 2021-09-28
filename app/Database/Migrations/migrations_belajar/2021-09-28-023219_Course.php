6bea3067338ae5f9e4ebeef5c0b6b97c3f3e8f0d
Alfimadani <alfimadani100@gmail.com>
2021-09-28 10:39:01 +0800

    menambahkan file course pada database course.php

---
+++ app/Database/Migrations/migrations_belajar/2021-09-28-023219_Course.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Database\Migrations\migrations_belajar;
+
+use CodeIgniter\Database\Migration;
+
+class Course extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'course_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 15,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'course'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+
+        ]);
+        $this->forge->addKey('course_id', true);
+        $this->forge->createTable('course');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('course');
+    }
+}
